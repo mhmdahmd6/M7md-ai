5b2ecb0b8bb3ee263cc9a840c8114ec3f9970a4d
mhmdahmd6 <m7md14426@gmail.com>
2026-02-17 06:54:55 +0200

    to update

--- src/App.js
+++ src/App.js
@@ -64,7 +64,7 @@ const M7mdAIInterface = () => {
     setShowResult(false);
 
     try {
-     const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash-latest" });
+     const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
       
       let result;
       if (activeTool === 'prompt') {
