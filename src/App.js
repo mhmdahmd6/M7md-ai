e751afa9cac937d19e6ad5a168e441bafb62cf67
mhmdahmd6 <m7md14426@gmail.com>
2026-02-17 07:15:54 +0200

    to update

--- src/App.js
+++ src/App.js
@@ -64,7 +64,7 @@ const M7mdAIInterface = () => {
 
     try {
       // استخدام النسخة المستقرة من الموديل
-      const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
+     const model = genAI.getGenerativeModel({ model: "gemini-3-flash-preview" });
       
       let result;
       if (activeTool === 'prompt') {
